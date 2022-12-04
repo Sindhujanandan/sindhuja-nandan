da5e33da64897884b7642c2eb52feee1d898d2da
Sindhujanandan <102343819+Sindhujanandan@users.noreply.github.com>
2022-12-04 12:41:05 +0530

    Add files via upload

--- Question01.java
+++ Question01.java
@@ -1,29 +1,77 @@
-import java.util.HashMap;
 public class Question01 {
-    public static void main(String[] args) {
-        // create
-        HashMap<String,Integer>hm= new HashMap<>();
-        // insert-O(1)
-        hm.put("India", 100);
-        hm.put("china", 150);
-        hm.put("US", 50);
-        System.out.println(hm);
+    static int tree[];
+    public static void init(int n)
+    {
+        tree=new int[4*n];
+    }
+    public static int buildBST(int arr[],int i,int start,int end )//O(n)
+    {
+        if(start==end)
+        {
+            tree[i]=arr[start];
+            return arr[start];
+        }
+        int mid=(start+end)/2;
+        buildBST(arr, 2*i+1, start, mid);
+        buildBST(arr, 2*i+2, mid+1, end);
+        tree[i]=tree[2*i+1]+tree[2*i+2];
+        return tree[i];
+    }
+    public static int getSumUtil(int i,int si,int sj,int qi,int qj)
+    {
+        if(qj<=si||qi>=sj)
+        {
+            return 0;
+        }
+        else if(si>=qi&&sj<=qj){
+            return tree[i];
+        }else{
+            int mid=(si+sj)/2;
+            int left=getSumUtil(2*i+1, si,mid, qi, qj);
+            int right=getSumUtil(2*i+2, mid+1, sj, qi, qj);
+            return left+right;
+        }
 
-        //get-O(1)
-       int population= hm.get("India");
-       System.out.println(population);
-       System.out.println(hm.get("Indonesia"));
-       // containskey-O(1)
-       System.out.println(hm.containsKey("India"));
-       System.out.println(hm.containsKey("indonesia"));
-       // remove--O(1)
-       System.out.println(hm.remove("china"));
-       System.out.
-       println(hm);
-       // size
-       System.out.println(hm.size());
-       // Is empty
-       hm.clear();
-       System.out.println(hm.isEmpty());
+    }
+    public static int getSum(int arr[],int qi,int qj)
+    {
+        int n=arr.length;
+        return getSumUtil(0, 0, n-1, qi, qj);
+    }
+    public static void updateUtil(int i,int si,int sj,int idx ,int diff)
+{
+    if(idx>sj||idx<si)
+    {
+        return;
+    }
+    tree[i]+=diff;
+    if(si!=sj)
+    {
+        int mid=(si+sj)/2;
+        updateUtil(2*i+1, si, mid, idx, diff);
+        updateUtil(2*i+2, mid+1, sj, idx, diff);
+    }
+}
+    public static void update(int arr[],int idx,int newVal)
+    {
+        int n=arr.length;
+        int diff=newVal-arr[idx];
+        arr[idx]=newVal;
+        updateUtil(0, 0,n-1,idx,diff);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8};
+        int n=arr.length;
+        init(n);
+        buildBST(arr, 0, 0, n-1);
+        for(int i=0;i<tree.length;i++)
+        {
+            System.out.print(tree[i]+" ");
+        }
+        System.out.println();
+        System.out.println( getSum(arr,2,5));
+        update(arr, 2, 2);
+        System.out.println(getSum(arr,2,5));
+       
     }
 }
--- Question2.java
+++ Question2.java
@@ -1,44 +1,80 @@
-import java.util.Arrays;
-
 public class Question2 {
-    public static int countWays(int n,int ways[])
+    static int tree[];
+    public static void init(int n)
     {
-        if(n==0)
-        {
-            return 1;
-        }
-        if(n<0){
-            return 0;
-        }
-        if(ways[n]!=-1)
-        {
-            return ways[n];
-        }
-        ways[n]=countWays(n-1, ways)+countWays(n-2, ways);
-        return ways[n];
+        tree=new int[4*n];
     }
-    public static int countWaysTabular(int n)
+    public static void buildTree(int i,int si,int sj,int arr[]) {//O(n)
+        if(si==sj)
+        {
+            tree[i]=arr[si];
+            return;
+        }
+        int mid=(si+sj)/2;
+        buildTree(2*i+1, si, mid, arr);
+        buildTree(2*i+2, mid+1, sj, arr);
+
+        tree[i]=Math.max(tree[2*i+1],tree[2*i+2]);
+    }
+    public static int getMax(int arr[],int qi,int qj)
     {
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        for(int i=1;i<=n;i++)
-        {
-            if(i==1)
-            {
-                dp[i]=dp[i-1]+0;
-            }else
-            {
-                dp[i]=dp[i-1]+dp[i-2];
-            }
-           
-        }
-        return dp[n];
+        int n=arr.length;
+        return getMaxUtil(0,0,n-1,qi,qj);
     }
+    public static int getMaxUtil(int i,int si,int sj,int qi,int qj)  {//O(n)
+        if(si>qj||sj<qi){
+            return Integer.MIN_VALUE;
+        }
+        else if(si>=qi && sj<=qj)
+        {
+            return tree[i];
+        }
+        else{
+            int mid=(si+sj)/2;
+            int leftAns=getMaxUtil(2*i+1, si, mid, qi, qj);
+            int rightAns=getMaxUtil(2*i+2, mid+1, sj, qi, qj);
+            return Math.max(leftAns, rightAns);
+        }
+    }
+public static void update(int arr[],int idx,int newVAl)
+{
+    arr[idx]=newVAl;
+    int n=arr.length;
+    updateUtil(0, 0, n-1, idx, newVAl);
+}
+public static void updateUtil(int i,int si,int sj,int idx,int newVAl)
+{
+    if(idx<si|| idx>sj)
+    {
+        return ;
+    }
+    if(si==sj)
+    {
+        tree[i]=newVAl;
+    }
+    if(si!=sj)
+    {
+        tree[i]=Math.max(tree[i], newVAl);
+        int mid=(si+sj)/2;
+        updateUtil(2*i+1, si, mid, idx, newVAl);
+        updateUtil(2*i+2, mid+1, sj, idx, newVAl);
+    }
+    
+}
     public static void main(String[] args) {
-        int n=5;
-        int ways[]=new int[n+1];
-        Arrays.fill(ways, -1);
-        System.out.println(countWays(n, ways));
-        System.out.println(countWaysTabular(n));
+        int arr[]={6,8,-1,2,17,1,3,2,4};
+        int n=arr.length;
+        init(n);
+        buildTree(0, 0, n-1, arr);
+       // for(int i=0;i<tree.length;i++)
+        //{
+          //  System.out.print(tree[i]+" ");
+        //}
+       // System.out.println();
+        int max=getMax(arr,2,5);
+        System.out.println(max);
+        update(arr, 2, 20);
+         max=getMax(arr,2,5);
+        System.out.println(max);
     }
 }
