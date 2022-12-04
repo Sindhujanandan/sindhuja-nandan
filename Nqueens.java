4e0713ab6be3b8578206f3dd8934b165eaed159f
Sindhujanandan <102343819+Sindhujanandan@users.noreply.github.com>
2022-12-04 12:27:24 +0530

    Backtracking

---
+++ Nqueens.java
@@ -0,0 +1,83 @@
+public class Nqueens {
+    public static boolean isSafe(char board[][],int row,int col)
+    {// vertical up
+        for(int i=row-1;i>=0;i--)
+        {
+            if(board[i][col]=='Q')
+            {
+                return false;
+            }
+        }
+        // diag  left up
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
+        {
+      if(board[i][j]=='Q')
+      {
+        return false;
+      }
+    }
+      // diag right up
+      for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++)
+        {
+            if(board[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        return true;
+        }
+    public static boolean nQueens(char board[][],int row)
+    {   //base 
+        if(row==board.length)
+        {//printBoard
+           printBoard(board);
+            return true;
+        }
+        //colomn loop
+        for(int j=0;j<board.length;j++)
+        {
+            if(isSafe(board,row,j))
+            {
+                board[row][j]='Q';
+                if(nQueens(board, row+1))
+                {
+                    return true;
+                }
+                board[row][j]='x';
+            }
+        }
+        return false;
+    }
+    public static void printBoard(char [][]board)
+    {
+        System.out.println("---------chess board---------");
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board.length;j++)
+            {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=4;
+        char board[][]=new char [n][n];
+        for(int i=0;i<n;i++)
+        {
+    for(int j=0;j<n;j++)
+    {
+        board[i][j]='X';
+    }
+        }
+       if(nQueens(board,0))
+       {
+        System.out.println("solution is exist");
+       } 
+       else
+       {
+        System.out.println("solution is not exist");
+       }
+    }
+
+}
